27c47ca81db03f5719e5cc69c78583cd01bc32f6
Emtinan98 <102264151+Emtinan98@users.noreply.github.com>
2022-05-16 12:29:18 +0300

    ADDED NEW LAB

---
+++ lab_loop.py
@@ -0,0 +1,19 @@
+for num in range(45,210):
+    if num == 100:
+        continue
+    elif num == 206:
+        break
+    print("Number", num)
+
+
+while True:
+    input_user = int(input("What is the product of 7 * 24 ? "))
+    if input_user == 168:
+       print("You answered this Q Correctly ")
+       break
+    else:
+       print("Sorry your answer is false !")
+
+    
+    
+   
