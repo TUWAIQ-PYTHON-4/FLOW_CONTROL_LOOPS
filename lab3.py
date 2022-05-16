a464fb42d33f2c62543cc9a92bf5a7d06a0c2d2a
nourah <altamiminourahcs@gmail.com>
2022-05-16 12:22:56 +0300

    My message

---
+++ lab3.py
@@ -0,0 +1,19 @@
+
+for i in range (45,210):
+    if i==100 :
+        continue
+    elif i==205:
+        break
+    else:
+        print(i)
+        
+n=input("what is the product of 7 * 24 ?")      
+while True:
+    if n=="168":
+        print("You answered this Question correctly")
+        break 
+    else:
+        n=input("what is the product of 7 * 24 ?")      
+
+
+    
