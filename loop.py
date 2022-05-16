10158cf22316956c430d9d923ef3a016001cdb95
ali <mashbariali@gmail.com>
2022-05-16 13:53:41 +0300

    added naw file and named it

---
+++ loop.py
@@ -0,0 +1,18 @@
+
+
+for number in range(45, 210) :
+    if number == 100: 
+        continue
+    elif number == 205: 
+        break
+    print(number)
+
+
+
+answer=int(input("what is the product of 7 * 24 ?"))
+
+while answer !=168 :
+
+  answer=int(input("what is the product of 7 * 24 ?"))
+else:
+    print( "You answered this Question correctly")
