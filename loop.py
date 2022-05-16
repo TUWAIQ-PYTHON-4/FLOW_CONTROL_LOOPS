bcb153a2cfe159edfca5c231da62c79376d6df17
Faisal othman <Faisalalsneed@gmail.com>
2022-05-16 11:50:52 +0300

    file Loop

---
+++ loop.py
@@ -0,0 +1,24 @@
+from tkinter import N
+
+
+for i in range(45,210):
+    
+    if i ==205:
+      break
+    elif i==100:
+        continue
+    print(i)
+
+
+a=input("7*24:")
+i=0
+while i<1:
+    i+=1
+    if a == '168':
+        print("You answered this Question correctly")
+    else :
+       print("Your Answer is wrong try again..")    
+        
+
+
+
