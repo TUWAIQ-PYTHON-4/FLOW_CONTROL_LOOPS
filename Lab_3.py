01ef8d49306e42bcde997395aa0f6032eb462e29
mohamed sulaiman <mohamed@outlook.sa>
2022-05-16 13:14:12 +0300

    Creat a new file python

---
+++ Lab_3.py
@@ -0,0 +1,30 @@
+#Using range()
+for number in range(45, 210,):
+    
+    if number == 100:
+     
+     continue
+
+    elif number == 205:
+       
+      break
+    print (number)
+
+     
+
+#Using a while loop and input
+i= 0
+while i < 1 :
+    ask_user= input("What is the product of 7 * 24 ?")
+
+    if ask_user == '168' :
+      print ("You answered this Question correctly")
+      break
+    else: 
+        print ("Your Answer is wrong try again..")
+       
+
+
+
+
+
