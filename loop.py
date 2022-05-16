a460a662cf42c797eabe509b519080a7821985d0
Asma Abufayah <aaha44@gmail.com>
2022-05-16 12:51:24 +0300

    loop lab

---
+++ loop.py
@@ -0,0 +1,21 @@
+i=0
+for i in range(45,210):
+     
+     if i==100:
+       continue
+     elif i==205:
+        break
+     else:
+         print (i)
+
+while True :
+    num=int(input("what is the product of 7 * 24 ?"))
+
+    if num==168:
+         print ("You answered this Question correctly")
+         break          
+
+    else:
+
+         print("Your Answer is wrong try again..")
+
